9de82dbc220e5450c22cf4d8255f69c8139986ca
Vinicius Serra <vssm@cin.ufpe.br>
2020-10-26 20:11:07 -0300

    first mutation

--- Ackley Function/src/ackley.py
+++ Ackley Function/src/ackley.py
@@ -2,13 +2,19 @@ import random
 import numpy as np
 import itertools
 from lib.population import Population
+from lib.individual import Individual
 
 def main():
-    params = {'population_size': 2}
-    population = Population(params)
-    population.print_population()
-    n_generation = 0
+    #params = {'population_size': 2}
+    #population = Population(params)
+    #population.print_population()
+    #n_generation = 0
 
+    ind = Individual()
+    
+    print(ind.x[:5], ind.sigma[:5])
+    ind.mutate()
+    print(ind.x[:5], ind.sigma[:5])
     
     """
     while population.get_fittest_individual().fitness() != 0:
--- Ackley Function/src/lib/individual.py
+++ Ackley Function/src/lib/individual.py
@@ -5,19 +5,24 @@ class Individual:
     def __init__(self, n = 30, c1 = 20, c2 = 0.2, c3 = 2*np.pi):
         self.n = n
         self.x = [random.uniform(-15.0, 15.0) for i in range(n)]
+        self.sigma = [1 for i in range(n)]
         self.c1 = c1
         self.c2 = c2
         self.c3 = c3
-            
+        self.t1 = 1 / np.sqrt(n)
+        self.t2 = 1 / np.sqrt(np.sqrt(n))
+        self.eps = 1e-6
+
     def fitness(self):
         n, x, c1, c2, c3 = self. n, self.x, self.c1, self.c2, self.c3
 
         a = -c1 * np.exp(-c2 * np.sqrt((1 / n) * sum(map(lambda y: y*y, x))))
-        b = -np.exp(1 / n * sum(map(lambda y : np.cos(c3 * y), x))) + c1 + 1
+        b = -np.exp(1 / n * sum(map(lambda y : np.cos(c3 * y), x))) + c1 + np.exp(1)
         return a + b
 
     def __lt__(self, other):
         return self.fitness() < other.fitness()
 
-    def mutation(self, method):
-        return self
+    def mutate(self):
+        self.sigma = [max(s * np.exp(self.t1 * np.random.normal(0, 1) + self.t2 * np.random.normal(0, 1)), self.eps) for s in self.sigma]
+        self.x = [x + sigma * np.random.normal(0, 1) for (x, sigma) in zip(self.x, self.sigma)]
